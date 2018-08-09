7b9a8b28b9604d9b1e26fb58eb05a288d2921457
Duy Nguyen <dtn9797@gmail.com>
2018-08-08 19:41:10 -0500

    Updated  buid.gradle to the current version , but ArticleDetailFragment.java still doesn't recognize CollapsingToolbarLayouts

--- XYZReader/src/main/java/com/example/xyzreader/ui/ArticleDetailFragment.java
+++ XYZReader/src/main/java/com/example/xyzreader/ui/ArticleDetailFragment.java
@@ -213,7 +213,7 @@ public class ArticleDetailFragment extends Fragment implements
             mRootView.setVisibility(View.VISIBLE);
             mRootView.animate().alpha(1);
             titleView.setText(mCursor.getString(ArticleLoader.Query.TITLE));
-            ((CollapsingToolbarLayout) mRootView.findViewById(R.id.detailedF_collapsing_toolbar)).setTitle(mCursor.getString(ArticleLoader.Query.TITLE));
+
             Date publishedDate = parsePublishedDate();
             if (!publishedDate.before(START_OF_EPOCH.getTime())) {
                 bylineView.setText(Html.fromHtml(
